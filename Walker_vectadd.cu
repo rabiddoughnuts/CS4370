197c524f972a8c0a146917dce4fcdfc45610df12
rabiddoughnuts <111888263+rabiddoughnuts@users.noreply.github.com>
2024-09-23 14:51:15 -0500

    Add timers, and matrix printing

--- Walker_vectadd.cu
+++ Walker_vectadd.cu
@@ -2,6 +2,7 @@
 
 #include <iostream>
 #include <cuda_runtime.h>
+#include <chrono>
 
 using namespace std;
 
@@ -9,6 +10,7 @@ void init_matrix(int *A, int *B, int N);
 void add_matrix_cpu(int *A, int *B, int *C, int N);
 __global__ void add_matrix_gpu(int *A, int *B, int *C, int N);
 void compare_matrices(int *cpu_result, int *gpu_result, int N);
+void print_matrix(int *matrix, int N, const char *name);
 
 int main(){
     int N, block_size;
@@ -18,7 +20,7 @@ int main(){
     cin >> N;
 
     // Get bloack size from user
-    cout << "enter the block size for CUDA: ";
+    cout << "Enter the block size for CUDA: ";
     cin >> block_size;
 
     // Allocate memory for matrices
@@ -28,7 +30,19 @@ int main(){
     int *C_gpu = new int[N * N];
 
     init_matrix(A, B, N);
+
+    print_matrix(A, N, "Matrix A");
+    print_matrix(B, N, "Matrix B");
+
+    auto start_cpu = chrono::high_resolution_clock::now();
+
     add_matrix_cpu(A, B, C_cpu, N);
+
+    auto ent_cpu = chrono::high_resolution_clock::now();
+    chrono::duration<float, milli> duration_cpu = end_cpu - start_cpu;
+    cout << "CPU time: " << duration_cpu.count() << " ms" << endl;
+
+    print_matrix(C_cpu, N, "Matrix C (CPU)");
 
     int *d_A, *d_B, *d_C;
     cudaMalloc((void **)&d_A, N * N * sizeof(int));
@@ -41,9 +55,25 @@ int main(){
     dim3 dimBlock(block_size, block_size);
     dim3 dimGrid((N + block_size - 1) / block_size, (N + block_size - 1) / block_size);
 
+    cudaEvent_t start_gpu, stop_gpu;
+    cudaEventCreate(&start_gpu);
+    cudaEventCreate(&stop_gpu);
+
+    cudaEventRecord(start_gpu);
+
     add_matrix_gpu<<<dimGrid, dimBlock>>>(d_A, d_B, d_C, N);
 
+    cudaEventRecord(stop_gpu);
+    cudaEventSynchronise(stop_gpu);
+
+    float milliseconds = 0;
+    cudaEventElapsedTimed(milliseconds, start_gpu, stop_gpu);
+
+    cout << "GPU time: " << milliseconds << " ms" << endl;
+
     cudaMemcpy(C_gpu, d_C, N * N * sizeof(int), cudaMemcpyDeviceToHost);
+
+    print_matrix(C_gpu, N, "Matrix C (GPU)");
 
     compare_matrices(C_cpu, C_gpu, N);
 
@@ -97,4 +127,23 @@ void compare_matrices(int *cpu_result, int *gpu_result, int N){
         }
     }
     cout << "CPU and GPU results match!" << endl;
+}
+
+void print_matrix(int *matrix, int N, const char *name){
+    cout << name << ":" << endl;
+    if (N <= 20){
+        for(int i = 0; i < N; i++){
+            for(int j = 0; j < N; j++){
+                cout << matrix[i * N + j] << " ";
+            }
+            cout << endl;
+        }
+    } else {
+        cout << "Matrix too large, printing only the first row:" << endl;
+        for(int j = 0; j < N; j++){
+            cout << matrix[j] << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
 }
